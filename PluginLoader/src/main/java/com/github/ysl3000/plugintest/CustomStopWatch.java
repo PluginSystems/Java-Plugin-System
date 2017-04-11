0b57e28ea3bbaca4a23e1680b26924ac5938ce33
Yannick <yannicklamprecht@live.de>
2017-04-11 18:57:22 +0200

    - added TestPlugin module - moved PluginLoader to submodule

---
+++ PluginLoader/src/main/java/com/github/ysl3000/plugintest/CustomStopWatch.java
@@ -0,0 +1,12 @@
+package com.github.ysl3000.plugintest;
+
+import org.springframework.util.StopWatch;
+
+/**
+ * Created by ysl3000
+ */
+public class CustomStopWatch {
+
+    private StopWatch stopWatch;
+
+}
--- TestPlugin/src/main/java/com/github/ysl3000/TwitchTestPlugin.java
+++ TestPlugin/src/main/java/com/github/ysl3000/TwitchTestPlugin.java
@@ -1,26 +1,27 @@
-package com.github.ysl3000.plugintest;
+package com.github.ysl3000;
+
 
 import com.github.ysl3000.pluginsystem.IPlugin;
 
 /**
  * Created by ysl3000
  */
-public class JavaPlugin implements IPlugin {
-
-
+public class TwitchTestPlugin implements IPlugin {
     @Override
     public String getPluginIdentity() {
-        return "MySpecialIdentity";
+        return "CustomTwitchTestPlugin2";
     }
 
     @Override
     public void onEnable() {
-        System.out.println("Test enable");
+
+        System.out.println("Testing onEnable");
+
     }
 
     @Override
     public void onDisable() {
 
-        System.out.println("Test disable");
+        System.out.println("Testing onDisable");
     }
 }
