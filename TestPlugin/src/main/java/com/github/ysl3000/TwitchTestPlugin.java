ef5f4ec26a33f632c5a80f74bf0da474aec2e68d
Yannick <yannicklamprecht@live.de>
2017-04-12 11:41:44 +0200

    - moved TwitchAPI to testPlugin

--- TestPlugin/src/main/java/com/github/ysl3000/TwitchTestPlugin.java
+++ TestPlugin/src/main/java/com/github/ysl3000/TwitchTestPlugin.java
@@ -2,6 +2,10 @@ package com.github.ysl3000;
 
 
 import com.github.ysl3000.pluginsystem.IPlugin;
+import com.github.ysl3000.twitchapi.TwitchAPI;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 
 /**
  * Created by ysl3000
@@ -14,6 +18,18 @@ public class TwitchTestPlugin implements IPlugin {
 
     @Override
     public void onEnable() {
+
+        TwitchAPI twitchAPI = new TwitchAPI("iiwsu6n5r8qiu1cug6zwupldjkfyn3");
+
+        try {
+            System.out.println("Spiel: " + twitchAPI
+                    .getSyncTwitchConnector()
+                    .connectToStream("deadpine").getStream().getGameName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Testing onEnable");
 
